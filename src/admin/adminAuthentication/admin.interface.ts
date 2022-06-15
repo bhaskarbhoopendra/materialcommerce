c58485804ea78dd75c69b173228a652000e335d0
Akshat Kumar <kumar.akshat29@gmail.com>
2022-06-16 02:50:18 +0530

    Admin Authentication

---
+++ src/admin/adminAuthentication/admin.interface.ts
@@ -0,0 +1,8 @@
+interface Iadmin {
+  _id: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+export default Iadmin;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
+import AdminAuthenticationController from "./admin/adminAuthentication/adminAuthentication.controller";
 import App from "./app";
 import GoogleAuthController from "./googleauth/googleauth.controller";
 
-const app = new App([new GoogleAuthController()]);
+const app = new App([new GoogleAuthController(),
+   new AdminAuthenticationController()]);
 
 app.listen();
