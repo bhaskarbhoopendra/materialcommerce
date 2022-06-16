ffd0b96ddbe8912730dded6f26529eb5c8423fa0
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-06-16 19:15:31 +0530

    adding pincodetype

--- src/exceptions/authenticationTokenMissingException.ts
+++ src/exceptions/authenticationTokenMissingException.ts
@@ -7,3 +7,23 @@ class AuthenticationTokenMissingException extends HttpException {
 }
 
 export default AuthenticationTokenMissingException;
+
+// import { Router, Request, Response } from "express";
+// import Controller from "../../interfaces/controller.interface";
+
+// class PincodeTypeController implements Controller {
+//   path = "/pincodetype";
+//   router = Router();
+//   constructor() {
+//     this.initializeRoutes();
+//   }
+
+//   private initializeRoutes() {
+//     this.router.post(`${this.path}/create`, this.createPincodeType);
+//   }
+
+//   private createPincodeType = async (
+//     request: Request,
+//     response: Response
+//   ) => {};
+// }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import AdminAuthenticationController from "./admin/adminAuthentication/adminAuthentication.controller";
 import App from "./app";
+import PincodeTypeController from "./freightrate/pincodetype/pincodetype.controller";
 import ZoneController from "./freightrate/zone/zone.controller";
 import GoogleAuthController from "./googleauth/googleauth.controller";
 
@@ -8,6 +9,7 @@ const app = new App([
   new GoogleAuthController(),
   new AdminAuthenticationController(),
   new ZoneController(),
+  new PincodeTypeController(),
 ]);
 
 app.listen();
