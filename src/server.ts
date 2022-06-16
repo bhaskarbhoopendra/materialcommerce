6aab7c43609130401e00b9b39092ca639afcc641
Anas <anas0shah0@gmail.com>
2022-06-16 21:15:24 +0530

    added authentication controller

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import AdminAuthenticationController from "./admin/adminAuthentication/adminAuthentication.controller";
 import App from "./app";
+import AuthenticationController from "./authentication/authentication.controller";
 import PincodeTypeController from "./freightrate/pincodetype/pincodetype.controller";
 import ZoneController from "./freightrate/zone/zone.controller";
 import GoogleAuthController from "./googleauth/googleauth.controller";
@@ -10,6 +11,7 @@ const app = new App([
   new AdminAuthenticationController(),
   new ZoneController(),
   new PincodeTypeController(),
+  new AuthenticationController()
 ]);
 
 app.listen();
