05c53189cd6f7d7c03280f718cd455f8426ef122
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-06-17 19:06:55 +0530

    test

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -2,11 +2,11 @@ import passport from "passport";
 import IUser from "../user/user.interface";
 import UserModel from "../user/user.model";
 
-passport.serializeUser((user, done: any) => {
+passport.serializeUser((user, done) => {
   return done(null, user);
 });
 
-passport.deserializeUser(async (id: string, done: any) => {
+passport.deserializeUser(async (id, done) => {
   UserModel.findById(id, (err: Error, doc: IUser) => {
     // Whatever we return goes to the client and binds to the req.user property
     return done(null, doc);
