598d84675313e4a37bdad9c888d295267a749e0b
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-06-17 19:02:49 +0530

    adding db

--- src/config/google.ts
+++ src/config/google.ts
@@ -1,6 +1,5 @@
 import passport from "passport";
 import passportGoogleOAuth from "passport-google-oauth20";
-import AuthenticationService from "../authentication/authentication.service";
 import GoogleAuthService from "../googleauth/googleauth.service";
 import UserModel from "../user/user.model";
 
@@ -29,18 +28,13 @@ passport.use(
       };
 
       const googleAuthService = new GoogleAuthService();
-      const authService = new AuthenticationService();
       try {
         const currentUser = await UserModel.findOne({ googleId: profile.id });
         if (currentUser) {
-          const token = authService.createToken(currentUser);
-          const cookie = authService.createCookie(token);
-          return done(null, { currentUser, token, cookie });
+          return done(null, currentUser);
         } else {
           const user = await UserModel.create(newUser);
-          const token = authService.createToken(user);
-          const cookie = authService.createCookie(token);
-          return done(null, { user, token, cookie });
+          return done(null, user);
         }
       } catch (error) {
         return done(error, false);
