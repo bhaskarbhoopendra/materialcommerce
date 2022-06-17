550149ae7472c016a076a51f5375fa21df56646c
Anas <anas0shah0@gmail.com>
2022-06-17 20:42:38 +0530

    added vendor controller in server

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,15 @@ import AuthenticationController from "./authentication/authentication.controller
 import PincodeTypeController from "./freightrate/pincodetype/pincodetype.controller";
 import ZoneController from "./freightrate/zone/zone.controller";
 import GoogleAuthController from "./googleauth/googleauth.controller";
+import VendorAuthenticationController from "./vendor/vendor.authentication.controller";
 
 const app = new App([
   new GoogleAuthController(),
   new AdminAuthenticationController(),
   new ZoneController(),
   new PincodeTypeController(),
-  new AuthenticationController()
+  new AuthenticationController(),
+  new VendorAuthenticationController()
 ]);
 
 app.listen();
