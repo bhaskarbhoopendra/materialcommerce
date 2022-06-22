37163205e2b181973f8efcccee919c3789864d4c
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-06-23 02:56:07 +0530

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,13 @@ class App {
         credentials: true,
       })
     );
+    this.app.use((req, res, next) => {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
+      next();
+    });
     this.app.use(cookieParser());
     this.app.use(
       session({
