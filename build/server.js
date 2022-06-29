bfa57414724725f3409b308825951c91242a5beb
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-06-29 16:39:06 +0530

    adding new files

--- build/server.js
+++ build/server.js
@@ -8,8 +8,6 @@ const admin_controller_1 = __importDefault(require("./admin/admin.controller"));
 const adminAuthentication_controller_1 = __importDefault(require("./admin/adminAuthentication/adminAuthentication.controller"));
 const app_1 = __importDefault(require("./app"));
 const authentication_controller_1 = __importDefault(require("./authentication/authentication.controller"));
-const freightrate_controller_1 = __importDefault(require("./freightrate/freightrate.controller"));
-const pincodetype_controller_1 = __importDefault(require("./freightrate/pincodetype/pincodetype.controller"));
 const zone_controller_1 = __importDefault(require("./freightrate/zone/zone.controller"));
 const googleauth_controller_1 = __importDefault(require("./googleauth/googleauth.controller"));
 const vendor_authentication_controller_1 = __importDefault(require("./vendor/vendor.authentication.controller"));
@@ -18,11 +16,9 @@ const app = new app_1.default([
     new googleauth_controller_1.default(),
     new adminAuthentication_controller_1.default(),
     new zone_controller_1.default(),
-    new pincodetype_controller_1.default(),
     new authentication_controller_1.default(),
     new vendor_authentication_controller_1.default(),
     new warehouse_controller_1.default(),
     new admin_controller_1.default(),
-    new freightrate_controller_1.default(),
 ]);
 app.listen();
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import AdminController from "./admin/admin.controller";
 import AdminAuthenticationController from "./admin/adminAuthentication/adminAuthentication.controller";
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
-import FreightRateController from "./freightrate/freightrate.controller";
-import PincodeTypeController from "./freightrate/pincodetype/pincodetype.controller";
 import ZoneController from "./freightrate/zone/zone.controller";
 import GoogleAuthController from "./googleauth/googleauth.controller";
 import VendorAuthenticationController from "./vendor/vendor.authentication.controller";
@@ -14,12 +12,10 @@ const app = new App([
   new GoogleAuthController(),
   new AdminAuthenticationController(),
   new ZoneController(),
-  new PincodeTypeController(),
   new AuthenticationController(),
   new VendorAuthenticationController(),
   new WarehouseController(),
   new AdminController(),
-  new FreightRateController(),
 ]);
 
 app.listen();
